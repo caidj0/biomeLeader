b1b8745bc0a1cd4062842e360a8066d160fd0bf1
caidj0 <51256838+caidj0@users.noreply.github.com>
2023-07-29 23:19:34 +0800

    Initial commit

---
+++ src/main/java/com/caidj0/biomeleader/BiomeLeaderClient.java
@@ -0,0 +1,10 @@
+package com.caidj0.biomeleader;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BiomeLeaderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
